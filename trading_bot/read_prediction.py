28e3ccfd70480bbddaeaeacaf67306071a73ab6c
Williams <williamsagbanoma@gmail.com>
2025-07-31 16:16:45 +0100

    Switch to google.oauth2.service_account in read_prediction

--- trading_bot/read_prediction.py
+++ trading_bot/read_prediction.py
@@ -2,13 +2,17 @@ import os
 import json
 import gspread
 
+from google.oauth2.service_account import Credentials
+
 def get_latest_5min_prediction(sheet_name='Allora_Model_Performance_Report', worksheet_name='5min Forecast'):
 
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
     google_creds = json.loads(os.environ.get("GOOGLE_CREDS_JSON"))
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(google_creds, scope)
+    creds = Credentials.from_service_account_info(google_creds, scopes=scope)
     client = gspread.authorize(creds)
-
     sheet = client.open(sheet_name)
 
     worksheet = sheet.worksheet(worksheet_name)
